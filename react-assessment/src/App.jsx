f9405db2559bb7e4990c26f59705858bfe894864
Gracelyn Prajna <gracelynprajna@gmail.com>
2023-03-28 00:28:26 -0400

    CLEANUP

--- react-assessment/src/App.jsx
+++ react-assessment/src/App.jsx
@@ -2,13 +2,11 @@ import useSWR from 'swr'
 import axios from 'axios';
  
 const fetcher = url => axios.get(url).then(res => res.data);
- 
 function App () {
-  const { data, error, isLoading } = useSWR("https://pokeapi.co/api/v2/pokemon", fetcher);
+  const { data, error } = useSWR("https://pokeapi.co/api/v2/pokemon", fetcher);
   return(
     <div>
-      {data &&
-        data.results.map((element) => (
+      {data && data.results.map((element) => (
           <div>
             <h1>{element.name}</h1>
           </div>
