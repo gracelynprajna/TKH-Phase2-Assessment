950a7f9372a058e6a826b75df691827cc285223f
Gracelyn Prajna <gracelynprajna@gmail.com>
2023-03-27 23:29:27 -0400

    I AM DONE!!

--- react-assessment/src/App.jsx
+++ react-assessment/src/App.jsx
@@ -1,33 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+import useSWR from 'swr'
+import axios from 'axios';
+ 
+const fetcher = url => axios.get(url).then(res => res.data);
+ 
+function App () {
+  const { data, error, isLoading } = useSWR("https://pokeapi.co/api/v2/pokemon", fetcher);
+  return(
+    <div>
+      {data &&
+        data.results.map((element) => (
+          <div>
+            <div>{element.name}</div>
+            <a href={element.url}> {element.url}</a>
+          </div>
+        ))}
     </div>
   )
 }
